bcb343beaaa72fd47c24b1414d1ab7629ae14131
Lucas NAURY <lucas.naury@etu.imt-nord-europe.fr>
2024-01-25 16:52:09 +0100

    Load map launch files

---
+++ pibot-control/launch/load_map.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import GroupAction
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+  return LaunchDescription([
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('nav2_bringup'),
+                        'launch/localization_launch.py')),
+      launch_arguments={
+        'map': os.path.join(
+                        get_package_share_directory('pibot-control'),
+                        'eiffel/pibot-control/config/map2/map.yaml'),
+        'autostart': 'True'
+        }.items()
+    )
+  ])
